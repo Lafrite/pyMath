20033d69ce5ab7fe999167a000bb8119e49c8597
Lafrite <lafrite@poneyworld.net>
2014-02-28 16:19:42 +0100

    add doc and change version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pyMath',
-      version='0.1dev',
+      version='0.2',
       description='Computing like a student',
       author='Benjamin Bertrand',
       author_email='lafrite@poneyworld.net',
