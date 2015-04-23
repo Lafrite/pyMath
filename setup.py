885514cbdb46a2420e7157669eed4ae72ae02eda
Lafrite <lafrite@poneyworld.net>
2015-04-23 08:13:11 +0200

    update setup.py and add install_requiers

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@
 from distutils.core import setup
 
 setup(name='pyMath',
-      version='0.2',
+      version='1.0',
       description='Computing like a student',
       author='Benjamin Bertrand',
       author_email='lafrite@poneyworld.net',
       packages=['pymath'],
+      install_requiers=['pyparsing', 'sympy'],
      )
