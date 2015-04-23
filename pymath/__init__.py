3ec1ed819a7570333480c9cdf7d88795d18c2a97
Lafrite <lafrite@poneyworld.net>
2015-04-23 08:42:04 +0200

    easier import

---
+++ pymath/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from .expression import Expression
+from .polynom import Polynom
+from .random_expression import random_str
+from .render import txt,tex
+
+
+# -----------------------------
+# Reglages pour 'vim'
+# vim:set autoindent expandtab tabstop=4 shiftwidth=4:
+# cursor: 16 del 
